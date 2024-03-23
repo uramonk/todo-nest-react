cbac4670274cf605293de35396fd4304fc122762
uramonk <kazura1985@gmail.com>
2024-03-23 13:56:02 +0900

    httpsで起動できるようにする

--- README.md
+++ README.md
@@ -5,3 +5,38 @@ https://docs.nestjs.com/
 https://zenn.dev/red_frasco/articles/d5b7ec71ceacf6
 https://github.com/tkcel/todo-app-nest-react
 https://zenn.dev/morinokami/articles/nestjs-overview
+
+# 準備
+
+## https用の証明書作成
+
+### mkcert
+
+```bash
+brew install mkcert
+```
+
+### 自己ルート認証局追加
+
+```bash
+mkcert -install
+```
+
+### 証明書作成
+
+```bash
+cd backend
+mkcert localhost
+```
+
+## インストール
+
+```bash
+npm install
+```
+
+# 実行
+
+```bash
+npm run start:dev
+```
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import * as fs from 'fs';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('./localhost-key.pem'),
+    cert: fs.readFileSync('./localhost.pem'),
+  };
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
   await app.listen(3001);
 }
 bootstrap();
